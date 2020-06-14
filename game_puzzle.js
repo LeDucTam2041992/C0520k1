4ff9fed3c3339100f39eed539d8a8b94e451c728
LeDucTam2041992 <you@example.com>
2020-06-14 09:39:36 +0700

    up

--- game_puzzle.js
+++ game_puzzle.js
@@ -1,10 +1,38 @@
 let nameAni =['monkey','funny-cat','panda_swap'];
 function changeIM(x) {
-    let a,b,img1;
+    let a,b,img;
         a = Math.floor(Math.random() * 3);
         b = x.src[x.src.indexOf('.') - 1];
-        img1 = 'images/' + nameAni[a] + '_part' + b + '.jpg';
-    x.src = img1;
+        img = 'images/' + nameAni[a] + '_part' + b + '.jpg';
+    x.src = img;
+    let name1 = document.getElementById('img1').src;
+    name1 = name1.substring(name1.indexOf('images/'),name1.indexOf('.')-6);
+    // console.log(name1);
+    let name2 = document.getElementById('img2').src;
+    name2 = name2.substring(name2.indexOf('images/'),name2.indexOf('.')-6);
+    // console.log(name2);
+    // console.log(name1==name2);
+    let name3 = document.getElementById('img3').src;
+    name3 = name3.substring(name3.indexOf('images/'),name3.indexOf('.')-6);
+    let name4 = document.getElementById('img4').src;
+    name4 = name4.substring(name4.indexOf('images/'),name4.indexOf('.')-6);
+    let name5 = document.getElementById('img5').src;
+    name5 = name5.substring(name5.indexOf('images/'),name5.indexOf('.')-6);
+    // console.log(name1==name2 && name2==name3 && name3==name4 && name4==name5);
+    if (name1==name2 && name2==name3 && name3==name4 && name4==name5) {
+        document.getElementById("img1").style="border: solid blue" ;
+        document.getElementById("img2").style="border: solid blue" ;
+        document.getElementById("img3").style="border: solid blue" ;
+        document.getElementById("img4").style="border: solid blue" ;
+        document.getElementById("img5").style="border: solid blue" ;
+    }
+    else {
+        document.getElementById("img1").style="" ;
+        document.getElementById("img2").style="" ;
+        document.getElementById("img3").style="" ;
+        document.getElementById("img4").style="" ;
+        document.getElementById("img5").style="" ;
+    }
 }
 function setGame() {
     let a;
