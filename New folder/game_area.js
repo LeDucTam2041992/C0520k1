1d190cb67d3af5067031bd8f1009928ebeee7bd9
LeDucTam2041992 <you@example.com>
2020-06-25 17:11:44 +0700

    sx lai file

--- New folder/game_area.js
+++ New folder/game_area.js
@@ -21,3 +21,62 @@ var myGameArea = {
         clearInterval(this.interval);
     }
 }
+function updateGameArea() {
+    function shoot() {
+        timeShoot++;
+        if (timeShoot >= 10) {
+            let bullet = new component(bulletWidth, bulletHeight, 'red',
+                (spaceShip.x + (spaceShip.width - bulletWidth)/2), spaceShip.y);
+            bullets.push(bullet);
+            timeShoot = 0;
+        }
+    }
+    for (let i = 0; i < enemy.length; i++) {
+        if (spaceShip.crashWith(enemy[i])) {
+            myGameArea.stop();
+            return;
+        }
+    }
+    myGameArea.clear();
+    myBackGround.speedY = 1;
+    myBackGround.newPos();
+    myBackGround.update();
+    for (let i = 0; i < enemy.length; i++) {
+        if (enemy[i].y >= mapHeight) {
+            enemy[i].x = Math.floor(Math.random()*(mapWidth - enemyWidth));
+            enemy[i].y = -Math.floor(Math.random()*mapHeight);
+        }
+        for (let j = 0; j< bullets.length; j++){
+            if(enemy[i].crashWith(bullets[j])) {
+                enemy[i].x = Math.floor(Math.random()*(mapWidth - enemyWidth));
+                enemy[i].y = -Math.floor(Math.random()*mapHeight);
+                bullets.splice(j,1);
+                score++;
+                if (score%10==0) enemySpeed+=1;
+            }
+        }
+        enemy[i].speedY = enemySpeed;
+        enemy[i].newPos();
+        enemy[i].update();
+    }
+    for (let i = 0; i < bullets.length; i++) {
+        if (bullets[i].y < 0 ) {
+            bullets.splice(i, 1);
+        } else {
+            bullets[i].speedY = -10;
+            bullets[i].newPos();
+            bullets[i].update();
+        }
+    }
+    myScore.point = 'Score = '+score;
+    myScore.update();
+    spaceShip.speedX = 0;
+    spaceShip.speedY = 0;
+    if (myGameArea.keys && myGameArea.keys[37]) {spaceShip.speedX = -2;}
+    if (myGameArea.keys && myGameArea.keys[39]) {spaceShip.speedX = 2;}
+    if (myGameArea.keys && myGameArea.keys[38]) {spaceShip.speedY = -2;}
+    if (myGameArea.keys && myGameArea.keys[40]) {spaceShip.speedY = 2;}
+    if (myGameArea.keys && myGameArea.keys[32]) { shoot(); }
+    spaceShip.newPos();
+    spaceShip.update();
+}
